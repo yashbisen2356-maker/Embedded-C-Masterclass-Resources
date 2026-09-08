091640bea704a05253cf25050560d4a774416f53
yashbisen2356-maker <yashbisen2356@gmail.com>
2026-09-08 22:56:17 +0530

    Add files via upload

---
+++ Day 5/Day 6/Project/PROJECT2/Inc/math.h
@@ -0,0 +1,12 @@
+#ifndef MATH_H_
+#define MATH_H_
+
+
+
+int math_add(int a , int b );
+int math_sub(int a , int b );
+long long int math_mul(int a , int b );
+float math_div(float a , float b );
+
+
+#endif
