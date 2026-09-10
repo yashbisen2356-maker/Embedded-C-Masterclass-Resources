11d8ff48feb929f033cba4ad9fad90f2fae1f091
yashbisen2356-maker <yashbisen2356@gmail.com>
2026-09-10 22:55:21 +0530

    Add files via upload

---
+++ Day 6/Project/PROJECT2/Src/math.c
@@ -0,0 +1,23 @@
+int math_add(int a , int b ){
+
+
+	return a+b;
+}
+int math_sub(int a , int b ){
+
+
+	return a-b;
+
+}
+long long int math_mul(int a , int b ){
+
+	return (long long int )a*b;
+}
+
+
+
+float math_div(float  a , float  b ){
+
+	return (float) a/b;
+
+}
